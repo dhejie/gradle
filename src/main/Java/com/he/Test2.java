317ddeebace6e11a9c80a4a0f96a32a8328dc282
何进结 <dhejie123@163.com>
2019-11-10 19:44:55 +0800

    函数式接口和stream流的简单应用

---
+++ src/main/Java/com/he/Test2.java
@@ -0,0 +1,13 @@
+package com.he;
+
+/**
+ * @ProjectName: gradle
+ * @Package: com.he
+ * @ClassName: Test2
+ * @Author: dhejie
+ * @CreateDate: 2019/11/10 18:33
+ * @UpdateDate: 2019/11/10 18:33
+ * @Version: 1.0
+ */
+public class Test2 {
+}
